1a2cc29a6cdcb90592310d43f8286470fd42d4e1
omaneh <122321976+omaneh@users.noreply.github.com>
2023-05-16 19:34:57 -0400

    add --a

--- AllSlashCommands/ModerationCommands.cs
+++ AllSlashCommands/ModerationCommands.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using DSharpPlus;
 using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
 using DSharpPlus.SlashCommands;
 namespace SISDiscordBot.AllSlashCommands;
 
@@ -110,7 +111,7 @@ public class ModerationCommands : ApplicationCommandModule
 	}
 
 	[SlashCommand("add_Role", "Create a new role- no permissions by default")]
-	public async Task CreateRole(InteractionContext ctx, [Option("Name", "What is the name of the role?")] string name, [Option("Color", "What color would you like the tag to be?")] string color)
+	public async Task CreateRole(InteractionContext ctx, [Option("Name", "What is the name of the role?")] string name, [Option("Color", "What color would you like the tag to be?")] string color, [Option("Emoji", "Please include an emoji for the role")] DiscordEmoji emoji)
 	{
 		await ctx.DeferAsync();
 
@@ -130,9 +131,11 @@ public class ModerationCommands : ApplicationCommandModule
 			};
 
 
-			await ctx.Guild.CreateRoleAsync(name, Permissions.None, chosenColor);
+            var role = await ctx.Guild.CreateRoleAsync(name, Permissions.None, chosenColor);
+            // Associate the emoji with the role
+            await role.ModifyAsync(properties => properties.Emoji = emoji);
 
-			var roleCreatedEmbed = new DiscordEmbedBuilder()
+            var roleCreatedEmbed = new DiscordEmbedBuilder()
 			{
 				Title = $"Role {name} successfully created",
 				Color = DiscordColor.Blurple
@@ -239,12 +242,55 @@ public class ModerationCommands : ApplicationCommandModule
 	}
 
 	[SlashCommand("Welcome_board", "Make a poll to allow users to assign themselves roles")]
-	public async Task WelcomeBoard(InteractionContext ctx, [Option("Message", "Provide a short description to welcome others into your discord")] string message)
+	public async Task WelcomeBoard(InteractionContext ctx, [Option("Message", "Provide a short title to welcome others into your discord")] string message)
 	{
 		await ctx.DeferAsync();
 
-		// come finish
-	}
+        try
+        {
+			var interactivity = ctx.Client.GetInteractivity();
+			DiscordRole[] allRoles = new DiscordRole[ctx.Guild.Roles.Count];
+			int pos = 0;
+			string body = string.Empty;
+			foreach(var role in ctx.Guild.Roles)
+			{
+				body += $"{role.Value.Emoji} {role.Value.Name}\n";
+				allRoles[pos] = role.Value;
+				pos++;
+			}
+
+
+            var pollMessage = new DiscordMessageBuilder()
+                .AddEmbed(new DiscordEmbedBuilder()
+                .WithTitle($"{message}")
+                .WithDescription("React to this message to get your roles!\n" + body)
+                .WithColor(DiscordColor.CornflowerBlue));
+
+            var reactionIncoming = await ctx.Channel.SendMessageAsync(pollMessage);
+
+            foreach (var role in allRoles)
+            {
+                await reactionIncoming.CreateReactionAsync(role.Emoji);
+            }
+
+            var result = await interactivity.CollectReactionsAsync(reactionIncoming);
+
+            foreach (var rxn in result)
+            {
+                foreach(var user in rxn.Users)
+				{
+					var member = (DiscordMember)user;
+					var targetRole = allRoles.Where(x => x.Emoji == rxn.Emoji).FirstOrDefault(); // I want this to return the role associated with the emoji
+					if (targetRole != null) { await member.GrantRoleAsync(targetRole); }
+				}
+
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
 
 
 }
